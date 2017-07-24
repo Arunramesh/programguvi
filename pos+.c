61c7a7cfb6b024d14dc654542251e8be81336c67
Arunramesh <30409046+Arunramesh@users.noreply.github.com>
2017-07-24 17:58:56 +0530

    Create pos+.c

---
+++ pos+.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("Enter the number");
+scanf("%d",&n);
+if(n>0)
+{
+printf("\n positive");
+}
+else if(n<0)
+{
+printf("\n negative");
+}
+else
+{
+printf("0")
+}
+return 0;
+}
